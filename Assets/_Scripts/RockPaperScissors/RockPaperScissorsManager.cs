b4cfbb46afa2e4a784854a65fee74fbc985b1a06
Ryan King <ryan@helloworldeng.com>
2018-07-27 13:30:12 -0700

    Added rock paper scissors prototype

---
+++ Assets/_Scripts/RockPaperScissors/RockPaperScissorsManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPaperScissorsManager : MonoBehaviour {
+
+	const int minJammage = 0;
+	const int maxJammage = 13;
+
+	public enum MusicalStyle {
+		Intense,
+		Grooving,
+		Halting
+	}
+
+	public RockPaperScissorsDisplay display;
+
+	MusicalStyle[] npcMusicalStyles = new MusicalStyle[]{ 	MusicalStyle.Grooving, 
+															MusicalStyle.Grooving, 
+															MusicalStyle.Intense, 
+															MusicalStyle.Intense, 
+															MusicalStyle.Halting, 
+															MusicalStyle.Halting, 
+															MusicalStyle.Grooving, 
+															MusicalStyle.Intense };
+	int currentNPCMusicalStyleIndex;
+
+	public MusicalStyle nextPlayerMusicalStyle;
+	int currentJammage;
+	int previousJammageModifier;
+
+	// Use this for initialization
+	void Start () {
+		currentJammage = (maxJammage - minJammage) / 2 + 1;
+		display.UpdateUI((currentJammage-minJammage)/(float)(maxJammage-minJammage));
+	}
+
+	public void PerformNextMove() {
+		var npcMusicalStyle = npcMusicalStyles[currentNPCMusicalStyleIndex];
+		var jammageMod = GetJammageModifierFromTwoMusicalStyles(nextPlayerMusicalStyle, npcMusicalStyle);
+		previousJammageModifier = jammageMod;
+		currentJammage += jammageMod;
+		if(currentJammage <= minJammage || currentJammage >= maxJammage)
+			LoseGame();
+
+		currentNPCMusicalStyleIndex++;
+		if(currentNPCMusicalStyleIndex >= npcMusicalStyles.Length)
+			WinGame();
+
+		Debug.Log("Jammage: " + currentJammage);
+		display.UpdateUI((currentJammage-minJammage)/(float)(maxJammage-minJammage), npcMusicalStyle, nextPlayerMusicalStyle);
+	}
+
+	void LoseGame() {
+		Debug.Log("You lost...");
+	}
+
+	void WinGame() {
+		Debug.Log("Done!");
+	}
+
+	int GetJammageModifierFromTwoMusicalStyles(MusicalStyle playerStyle, MusicalStyle npcStyle) {
+		// If the player played a trump style to the npc style, return +1
+		if(DoesStyleTrumpOtherStyle(playerStyle, npcStyle)) {
+			return 1;
+		} else if (DoesStyleTrumpOtherStyle(npcStyle, playerStyle)) {
+			return -1;
+		}
+		
+		// If they are tied, continue the previous jam
+		return previousJammageModifier;
+	}
+
+	bool DoesStyleTrumpOtherStyle(MusicalStyle style, MusicalStyle otherStyle) {
+		if( (style == MusicalStyle.Intense && otherStyle == MusicalStyle.Grooving) ||
+			(style == MusicalStyle.Grooving && otherStyle == MusicalStyle.Halting) ||
+			(style == MusicalStyle.Halting && otherStyle == MusicalStyle.Intense) )
+			return true;
+		
+		return false;
+	}
+}
---
+++ Assets/_Scripts/RockPaperScissors/RockPaperScissorsMusicTranslator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPaperScissorsMusicTranslator : MonoBehaviour {
+
+	const int numBeatsPerMove = 8;
+	// const int playerMoveBeatOffset = 8;
+	// const int numBeatsPerNPCMove = 16;
+	// const int virtuosoLoopCount = 3;
+
+	RockPaperScissorsManager gameManager;
+	// int numPlayerTracksPerMove = 4;
+	// List<int> playerMoveTracks = new List<int>();
+
+	int currentSongBeat = 0;
+	int currentPlayerTrack = 0;
+
+	// Maps the player's track indices to musical roles
+	RockPaperScissorsManager.MusicalStyle[] playerTrackStyles = { RockPaperScissorsManager.MusicalStyle.Halting, RockPaperScissorsManager.MusicalStyle.Grooving, RockPaperScissorsManager.MusicalStyle.Intense };
+
+	void Awake() {
+		gameManager = GetComponent<RockPaperScissorsManager>();
+	}
+
+	void DidPlayPlayerTrack(int playerTrackIndex) {
+		// Debug.Log("Adding player track " + playerTrackIndex + " at time " + Time.time);
+		currentPlayerTrack = playerTrackIndex;	
+		gameManager.nextPlayerMusicalStyle = playerTrackStyles[playerTrackIndex];
+	}
+
+	// void AddToPlayerMoveSet() {
+	// 	playerMoveTracks.Add(currentPlayerTrack);
+	// 	if(playerMoveTracks.Count >= 4) {
+	// 		InterpretPlayerMove(playerMoveTracks.ToArray());
+	// 		playerMoveTracks.Clear();
+	// 	}
+	// }
+
+	void DidStartNextBeat(SongPlayer.BeatUpdateInfo beatInfo) {
+		currentSongBeat++;
+		if(currentSongBeat % numBeatsPerMove == 0) {
+			gameManager.PerformNextMove();
+		}
+		// if(currentSongBeat % 4 == 0) {
+		// 	AddToPlayerMoveSet();
+		// }
+	}
+
+	// void InterpretPlayerMove(int[] moveTracks) {
+	// 	// Get the track roles for these moves
+	// 	var moveRoles = new List<LoopsRPGManager.MusicalRole>();
+	// 	var uniqueTracks = new HashSet<int>();
+	// 	foreach(var trackIndex in moveTracks) {
+	// 		moveRoles.Add(playerTrackRoles[trackIndex]);
+	// 		uniqueTracks.Add(trackIndex);
+	// 	}
+
+	// 	var finalRoleOfMove = moveRoles[moveRoles.Count-1];
+
+	// 	// If three different loops were involved in the move, the player goes all virtuosic
+	// 	if(uniqueTracks.Count >= virtuosoLoopCount) {
+	// 		rpgManager.PlayerVirtuoso();
+	// 	}
+	// 	// If the last role is different than the player's role, switch to that role
+	// 	if(finalRoleOfMove != rpgManager.playerRole) {
+	// 		rpgManager.PlayerChangeRoles(finalRoleOfMove);
+	// 	}
+	// 	// If only two loops were played and we ended in the same role, the player chilled
+	// 	if(finalRoleOfMove == rpgManager.playerRole && uniqueTracks.Count <= 2) {
+	// 		rpgManager.PlayerChill();
+	// 	}
+	// }
+}
