907d7530ee363c7b86e7c9c236bba419298c3b05
traviswhendrix <traviswhendrix@gmail.com>
2018-07-31 16:54:11 -0700

    added key controls and UI for toybox functions

--- Assets/_Scripts/Player/PlayerMidiController.cs
+++ Assets/_Scripts/Player/PlayerMidiController.cs
@@ -13,7 +13,14 @@ public class PlayerMidiController : MonoBehaviour {
 	public PlayerMouseSpringInput gateMouseInput;
 	public PlayerMouseSpringInput volumeMouseInput;
 	public PlayerMouseSpringInput instrumentMouseInput;
-	public PowerCircleAnimationController circleAnimator;
+	public PowerCircleAnimationController[] circleAnimator;
+	public KeyControlDisplay keyControlDisplay;
+	public bool mouseControls;
+	public Metronome metro;
+	
+	public float keyVolumeIncrement;
+	public int keyControlIndex = 1;
+	public int currentInstIndex;
 	
 	float songBPM = 240f;
 
@@ -31,6 +38,7 @@ public class PlayerMidiController : MonoBehaviour {
 	float savedGateValue = 0.5f;
 	float savedVolumeValue = 0.5f;
 	float savedInstrumentValue = 0.5f;
+	
 
 
 	// Use this for initialization
@@ -47,68 +55,194 @@ public class PlayerMidiController : MonoBehaviour {
 
 		Cursor.visible = false;
 		Cursor.lockState = CursorLockMode.Locked;
+		midiPlayer.synthBank.currentPlayerInstrument = playerInstruments[2];
+		currentInstIndex = 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		UpdateInstrument();
-		UpdateGate();
-		UpdateVolume();
+		if(mouseControls){
+			UpdateInstrument();
+			UpdateGate();
+			UpdateVolume();
+		}else{
+			UpdateKeyControls();
+		}
 
 		// Mathf.L
 		currentBPM = Mathf.Lerp(currentBPM, targetBPM, 5*Time.deltaTime);
 		midiPlayer.playbackRate = Mathf.Clamp(currentBPM / songBPM,0.3f,1.2f);
 	}
 
-	void UpdateGate() {
-		if(Input.GetButtonDown("Loop2")) {
-			gateArrow.isPowerActive = true;
-			gateMouseInput.SetAnchorWithValue(savedGateValue);
-
-		} else if(Input.GetButtonUp("Loop2")) {
-			gateArrow.isPowerActive = false;
+	void UpdateKeyControls(){
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			Debug.Log("LEFT ARROW PRESSED");
+			if(keyControlIndex > 0){
+				keyControlIndex--;
+				Debug.Log("keyControlIndex: " + keyControlIndex);
+				keyControlDisplay.UpdateDisplayValues();
+			}
 		}
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+			Debug.Log("RIGHT ARROW PRESSED");
+			if(keyControlIndex < 2){
+				keyControlIndex++;
+				Debug.Log("keyControlIndex: " + keyControlIndex);
+				keyControlDisplay.UpdateDisplayValues();
+			}
+		}
+		if(keyControlIndex == 0){
+			UpdateInstrument();
+		}else if(keyControlIndex == 1){
+			UpdateGate();
+		}else if(keyControlIndex == 2){
+			UpdateVolume();
+		}
+	}
 
-		if(Input.GetButton("Loop2")) {
-			savedGateValue = gateMouseInput.GetMouseValue();
-			int veloGate = maxGate - Mathf.RoundToInt((maxGate - minGate)*((savedGateValue + 1)/2f));
-			// Debug.Log("Gate: " + veloGate);
-			midiPlayer.trackGateVelocity = veloGate;
+	void UpdateGate() {
+		if(mouseControls){
+			if(Input.GetButtonDown("Loop2")) {
+				gateArrow.isPowerActive = true;
+				gateMouseInput.SetAnchorWithValue(savedGateValue);
+
+			} else if(Input.GetButtonUp("Loop2")) {
+				gateArrow.isPowerActive = false;
+			}
+
+			if(Input.GetButton("Loop2")) {
+				savedGateValue = gateMouseInput.GetMouseValue();
+				int veloGate = maxGate - Mathf.RoundToInt((maxGate - minGate)*((savedGateValue + 1)/2f));
+				// Debug.Log("Gate: " + veloGate);
+				midiPlayer.trackGateVelocity = veloGate;
+			}
+		}else{
+			if(Input.GetKeyDown(KeyCode.DownArrow)){
+				Debug.Log("GATE UP");
+				if(midiPlayer.trackGateVelocity < maxGate){
+					midiPlayer.trackGateVelocity++;
+				}
+				Debug.Log("Gate level:" + midiPlayer.trackGateVelocity);
+				keyControlDisplay.UpdateDisplayValues();				
+			}
+			if(Input.GetKeyDown(KeyCode.UpArrow)){
+				Debug.Log("GATE DOWN");
+				if(midiPlayer.trackGateVelocity > minGate){
+					midiPlayer.trackGateVelocity--;
+				}
+				Debug.Log("Gate level:" + midiPlayer.trackGateVelocity);
+				keyControlDisplay.UpdateDisplayValues();
+			}
 		}
 	}
 
 	void UpdateVolume() {
 		// Set the displays for each power
-		if(Input.GetButtonDown("Loop3")) {
-			volumeArrow.isPowerActive = true;
-			volumeMouseInput.SetAnchorWithValue(savedVolumeValue);
+		if(mouseControls){
+			if(Input.GetButtonDown("Loop3")) {
+				volumeArrow.isPowerActive = true;
+				volumeMouseInput.SetAnchorWithValue(savedVolumeValue);
 
-		} else if(Input.GetButtonUp("Loop3")) {
-			volumeArrow.isPowerActive = false;
-		}
+			} else if(Input.GetButtonUp("Loop3")) {
+				volumeArrow.isPowerActive = false;
+			}
 
-		if(Input.GetButton("Loop3")) {
-			savedVolumeValue = volumeMouseInput.GetMouseValue();
-			float volume = minVolume + (maxVolume - minVolume)*((savedVolumeValue + 1)/2f);
-			midiPlayer.playerVolume = volume;
+			if(Input.GetButton("Loop3")) {
+				savedVolumeValue = volumeMouseInput.GetMouseValue();
+				float volume = minVolume + (maxVolume - minVolume)*((savedVolumeValue + 1)/2f);
+				midiPlayer.playerVolume = volume;
+			}
+		}else{
+			
+			if(Input.GetKeyDown(KeyCode.DownArrow)){
+				float currentVolume = midiPlayer.playerVolume;
+				Debug.Log("VOLUME DOWN");
+				if(currentVolume > minVolume){
+					currentVolume -= keyVolumeIncrement;
+				}
+				Debug.Log("Volume level:" + currentVolume);				
+				midiPlayer.playerVolume = currentVolume;
+				keyControlDisplay.UpdateDisplayValues();
+			}
+			if(Input.GetKeyDown(KeyCode.UpArrow)){
+				float currentVolume = midiPlayer.playerVolume;
+				Debug.Log("VOLUME UP");
+				if(currentVolume < maxVolume){
+					currentVolume += keyVolumeIncrement;
+				}
+				Debug.Log("Volume level:" + currentVolume);				
+				midiPlayer.playerVolume = currentVolume;
+				keyControlDisplay.UpdateDisplayValues();
+			}
+			
 		}
+		
 	}
 
 	void UpdateInstrument() {
 		// Set the displays for each power
-		if(Input.GetButtonDown("Loop1")) {
-			instrumentArrow.isPowerActive = true;
-			instrumentMouseInput.SetAnchorWithValue(savedInstrumentValue);
-		} else if(Input.GetButtonUp("Loop1")) {
-			instrumentArrow.isPowerActive = false;
-		}
+		if (mouseControls){
+			if(Input.GetButtonDown("Loop1")) {
+				instrumentArrow.isPowerActive = true;
+				instrumentMouseInput.SetAnchorWithValue(savedInstrumentValue);
+			} else if(Input.GetButtonUp("Loop1")) {
+				instrumentArrow.isPowerActive = false;
+			}
 
-		if(Input.GetButton("Loop1")) {
-			savedInstrumentValue = instrumentMouseInput.GetMouseValue();
-			var instrumentIndex = Mathf.RoundToInt((playerInstruments.Length-1)*((savedInstrumentValue + 1)/2f));
-			int instrument = playerInstruments[instrumentIndex];
-			Debug.Log("Changing instrument to " + instrument);
-			midiPlayer.synthBank.currentPlayerInstrument = instrument;
+			if(Input.GetButton("Loop1")) {
+				savedInstrumentValue = instrumentMouseInput.GetMouseValue();
+				var instrumentIndex = Mathf.RoundToInt((playerInstruments.Length-1)*((savedInstrumentValue + 1)/2f));
+				int instrument = playerInstruments[instrumentIndex];
+				Debug.Log("Changing instrument to " + instrument);
+				midiPlayer.synthBank.currentPlayerInstrument = instrument;
+			}
+		}
+		else{
+			if(Input.GetKeyDown(KeyCode.DownArrow)){
+				//Setting Currentinstrument to the midiPLayer instrument
+				int currentInstrument = 1000;
+				for (int i = 0; i < playerInstruments.Length; i++){
+					if (playerInstruments[i] == midiPlayer.synthBank.currentPlayerInstrument){
+						currentInstrument = i;
+						continue;
+					}
+				}
+				if (currentInstrument == 1000){
+					Debug.LogError("Current instrument doesn't match midi instrument!");
+				}else{
+					Debug.Log("INST DOWN");
+					if(currentInstrument > 0){
+						currentInstrument--;
+					}
+					Debug.Log("Current Inst:" + currentInstrument);				
+					midiPlayer.synthBank.currentPlayerInstrument = playerInstruments[currentInstrument];
+					currentInstIndex = currentInstrument;
+					keyControlDisplay.UpdateDisplayValues();
+				}
+			}
+			if(Input.GetKeyDown(KeyCode.UpArrow)){
+				//Setting Currentinstrument to the midiPLayer instrument
+				int currentInstrument = 1000;
+				for (int i = 0; i < playerInstruments.Length; i++){
+					if (playerInstruments[i] == midiPlayer.synthBank.currentPlayerInstrument){
+						currentInstrument = i;
+						continue;
+					}
+				}
+				if (currentInstrument == 1000){
+					Debug.LogError("Current instrument doesn't match midi instrument!");
+				}else{
+					Debug.Log("INST UP");
+					if(currentInstrument < playerInstruments.Length - 1){
+						currentInstrument++;
+					}
+					Debug.Log("Current Inst:" + currentInstrument);				
+					midiPlayer.synthBank.currentPlayerInstrument = playerInstruments[currentInstrument];
+					currentInstIndex = currentInstrument;
+					keyControlDisplay.UpdateDisplayValues();
+				}
+			}
+			
 		}
 	}
 
@@ -122,7 +256,10 @@ public class PlayerMidiController : MonoBehaviour {
 
 	void StartSongWithBPM(float bpm) {
 		currentBPM = bpm;
-		circleAnimator.StartSong(bpm);
+		for (int i = 0; i < circleAnimator.Length; i++){
+			circleAnimator[i].StartSong(bpm);
+		}
+		metro.StartMetro(bpm);
 		animationManager.SetBPM(bpm);
 		animationManager.DidStartSong();
 		animationManager.UpdateGroove(1f);
