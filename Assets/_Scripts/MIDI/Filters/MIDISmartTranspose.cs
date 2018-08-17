9d46b1493438f07efb7e73586af6b195936e15e1
Ryan King <ryan@helloworldeng.com>
2018-08-16 17:18:29 -0700

    Fixed issue with note off events in transposition filter

--- Assets/_Scripts/MIDI/Filters/MIDISmartTranspose.cs
+++ Assets/_Scripts/MIDI/Filters/MIDISmartTranspose.cs
@@ -22,25 +22,43 @@ public class MIDISmartTranspose : MIDITrackFilter {
 	/// </summary>
 	List<TransposeRulesQueueElement> transposeRulesQueue = new List<TransposeRulesQueueElement>();
 
-	// If this is a note on event, then transpose it according to transpose rules and store in memory that it is "on" and has been translated
-	// If this is a note off event, then look to see what the corresponding note on event was and translate the note off event back
+	/// <summary>
+	/// When a note on event goes through the filter, the transposition gets saved here, where the key 
+	/// is the original pitch and the value is the pitch shift. When a note off event is received, it should
+	/// Check this array for how to transpose itself, and remove that entry from the dictionary
+	/// </summary>
+	Dictionary<int, int> currentNoteTranspositions = new Dictionary<int, int>();
 
+	/// <summary>
+	/// If this is a note on event, then transpose it according to transpose rules and store in memory that it is "on" and has been translated
+	/// If this is a note off event, then look to see what the corresponding note on event was and translate the note off event back
+	/// </summary>
 	public override MidiEvent[] FilterMidiEvents(MidiEvent[] events) {
 		var filteredEvents = new List<MidiEvent>();
 		
 		foreach(var midiEvent in events) {
 			var newMidiEvent = midiEvent.Duplicate();
 
-			var rule = GetTransposeRulesForSampleTimeAndUpdateQueue((int)midiEvent.deltaTime);
-			if(rule != null) {
+			if(midiEvent.midiChannelEvent == MidiHelper.MidiChannelEvent.Note_On) {
+				var rule = GetTransposeRulesForSampleTimeAndUpdateQueue((int)midiEvent.deltaTime);
+				if(rule != null) {
+					// pitch is parameter 1
+					int pitch = midiEvent.parameter1;
+					var pitchShift = rule.GetPitchShiftForPitch(pitch);
+					currentNoteTranspositions[pitch] = pitchShift;
+					pitch = pitch + pitchShift;
+
+					newMidiEvent.parameter1 = (byte)pitch;
+				}
+			} else if(midiEvent.midiChannelEvent == MidiHelper.MidiChannelEvent.Note_Off) {
 				// pitch is parameter 1
 				int pitch = midiEvent.parameter1;
-				var pitchShift = rule.GetPitchShiftForPitch(pitch);
+				var pitchShift = GetShiftForNoteOffEvent(pitch);
 				pitch = pitch + pitchShift;
 
 				newMidiEvent.parameter1 = (byte)pitch;
 			}
-
+			
 			filteredEvents.Add(newMidiEvent);
 		}
 		return filteredEvents.ToArray();
@@ -53,17 +71,29 @@ public class MIDISmartTranspose : MIDITrackFilter {
 
 	// Private
 	private TransposeRules GetTransposeRulesForSampleTimeAndUpdateQueue(int sampleTime) {
-		if(transposeRulesQueue.Count == 0) {
-			Debug.Log("L 0");
+		if(transposeRulesQueue.Count == 0 || transposeRulesQueue[0].startSampleTime > sampleTime) {
 			return null;
 		}
 
 		// First, make sure we dequeue elements that have passed
-		while(transposeRulesQueue.Count > 1 && transposeRulesQueue[1].startSampleTime < sampleTime) {
+		while(transposeRulesQueue.Count > 1 && transposeRulesQueue[1].startSampleTime <= sampleTime) {
 			transposeRulesQueue.RemoveAt(0);
 		}
 
-		Debug.Log("Transposing " + transposeRulesQueue[0].transposeRules.ToString());
 		return transposeRulesQueue[0].transposeRules;
+	}
+
+	// Given a note off midi event, transposes it to match the correct note on 
+	private int GetShiftForNoteOffEvent(int pitch) {
+		// Look through the unresolved note on events for one matching that pitch
+		if(currentNoteTranspositions.ContainsKey(pitch)) {
+			// Use that mapping to transpose this note as well
+			var shift = currentNoteTranspositions[pitch];
+			// Remove that element from the list
+			currentNoteTranspositions.Remove(pitch);
+			return shift;
+		}
+
+		return 0;
 	}
 }
