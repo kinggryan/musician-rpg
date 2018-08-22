babe1ddd1303006adffdc6493f78588d704845df
Ryan King <ryan@helloworldeng.com>
2018-08-22 13:45:45 -0700

    Added scorekeeper object. Scoring algorithm not implemented

--- Assets/_Scripts/Gameplay Managers/Scorekeeper.cs
+++ Assets/_Scripts/Gameplay Managers/Scorekeeper.cs
@@ -14,6 +14,12 @@ public class Scorekeeper : MonoBehaviour, IPlayerControllerListener, ISongUpdate
 
 	private List<SongRecord> playerSongRecord = new List<SongRecord>();
 	private List<SongRecord> npcSongRecord = new List<SongRecord>();
+	private List<SongPhrase> songPhrases = new List<SongPhrase>();
+
+	private AudioLoop currentNPCLoop;
+	private AudioLoop currentPlayerLoop;
+	private const int scoreEveryNumBeats = 4;
+	private const int scoreEveryNumBeatsOffset = 1;
 
 	// To make this work in the most dynamic way possible
 	// it should have a history of what the NPC and the player have played
@@ -30,18 +36,47 @@ public class Scorekeeper : MonoBehaviour, IPlayerControllerListener, ISongUpdate
 		songStructureManager.RegisterSongUpdateListener(this);
 	}
 
+	void Start() {
+		var songStructureManager = Object.FindObjectOfType<SongStructureManager>();
+		var sections = songStructureManager.songSections;
+		foreach(var section in sections) {
+			foreach(var phrase in section.phrases) {
+				songPhrases.Add(phrase);
+			}
+		}
+	}
+
 	// IAIListener
 	public void DidChangeAILoop(AIMIDIController ai, AudioLoop loop) {
-
+		currentNPCLoop = loop;
 	}
 
 	// IPlayerControllerListener
 	public void DidChangeLoop(AudioLoop newLoop) {
-
+		currentPlayerLoop = newLoop;
 	}
 
 	// ISongUpdateListener
 	public void DidStartNextBeat(SongStructureManager.BeatUpdateInfo beatInfo) {
+		// add a new song record
+		if(beatInfo.currentBeat > 0) {
+			var playerSongRecordEntry = new SongRecord();
+			playerSongRecordEntry.loop = currentPlayerLoop;
+			playerSongRecordEntry.startBeat = beatInfo.currentBeat - 1;
+			playerSongRecord.Add(playerSongRecordEntry);
 
+			var npcSongRecordEntry = new SongRecord();
+			npcSongRecordEntry.loop = currentNPCLoop;
+			npcSongRecordEntry.startBeat = beatInfo.currentBeat - 1;
+			npcSongRecord.Add(npcSongRecordEntry);
+		}
+
+		if(beatInfo.currentBeat > scoreEveryNumBeats && beatInfo.currentBeat % scoreEveryNumBeats == scoreEveryNumBeatsOffset) {
+			Score();
+		}
+	}
+
+	private void Score() {
+		// Do the scoring
 	}
 }
