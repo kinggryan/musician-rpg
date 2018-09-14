d61f111d29944c3b79d8923bc379b7c83d76c4be
Ryan King <ryan@helloworldeng.com>
2018-09-14 14:12:24 -0700

    Added menu and free play scene

---
+++ Assets/_Scripts/UI/MenuButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+	public string sceneToLoad;
+
+	// Use this for initialization
+	public void LoadScene() {
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
