a4b1e8928d8798130162c886f47e27f42fc884b0
Ryan King <ryan@helloworldeng.com>
2018-09-19 11:07:57 -0700

    Added basic display of ink story

---
+++ Assets/_Scripts/Overworld/Dialogue/DialogueManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+
+public class DialogueManager : MonoBehaviour {
+
+	[SerializeField]
+	private TextAsset inkJSONAsset;
+	private Story story;
+
+	[SerializeField]
+	private Canvas canvas;
+	[SerializeField]
+	private RectTransform pcDialogueOptionsParent;
+	[SerializeField]
+	private Text npcNameText;
+	[SerializeField]
+	private Text npcDialogueText;
+
+	[SerializeField]
+	private Button buttonPrefab;
+
+	void Awake () {
+		StartStory();
+	}
+
+	void StartStory () {
+		story = new Story (inkJSONAsset.text);
+		RefreshView();
+	}
+
+	void RefreshView () {
+		RemoveChoices ();
+
+		if (story.canContinue) {
+			string text = story.Continue ().Trim();
+			ShowNPCDialogue(text);
+		}
+
+		if(story.currentChoices.Count > 0) {
+			for (int i = 0; i < story.currentChoices.Count; i++) {
+				Choice choice = story.currentChoices [i];
+				Button button = CreateChoiceView (choice.text.Trim ());
+				button.onClick.AddListener (delegate {
+					OnClickChoiceButton (choice);
+				});
+			}
+		} else if(story.canContinue) {
+			Button choice = CreateChoiceView("(Continue)");
+			choice.onClick.AddListener(delegate{
+				RefreshView();
+			});
+		} else {
+			Button choice = CreateChoiceView("End of story.\nRestart?");
+			choice.onClick.AddListener(delegate{
+				StartStory();
+			});
+		}
+	}
+
+	void OnClickChoiceButton (Choice choice) {
+		story.ChooseChoiceIndex (choice.index);
+		RefreshView();
+	}
+
+	void ShowNPCDialogue (string text) {
+		npcDialogueText.text = text;
+	}
+
+	Button CreateChoiceView (string text) {
+		Button choice = Instantiate (buttonPrefab) as Button;
+		choice.transform.SetParent (pcDialogueOptionsParent, false);
+
+		Text choiceText = choice.GetComponentInChildren<Text> ();
+		choiceText.text = text;
+
+		return choice;
+	}
+
+	void RemoveChoices () {
+		int childCount = pcDialogueOptionsParent.transform.childCount;
+		for (int i = childCount - 1; i >= 0; --i) {
+			GameObject.Destroy (pcDialogueOptionsParent.transform.GetChild (i).gameObject);
+		}
+	}
+}
