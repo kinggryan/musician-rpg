381324b81c3594fcc6cc07663ec0f5a87938f90a
Ryan King <ryan@helloworldeng.com>
2018-10-15 16:33:08 -0700

    Fixed dialogue bubble position after end of musical encounter

---
+++ Assets/_Scripts/Overworld/Dialogue/UI/RotationLock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//<summary>
+// This class keeps the rotation of the object it is attached to equal to the camera's orientation at all times
+//</summary>
+public class RotationLock : MonoBehaviour {
+
+	private Quaternion lockedRotation;
+
+	// Use this for initialization
+	void Start () {
+		lockedRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = lockedRotation;
+	}
+}
---
+++ Assets/_Scripts/Overworld/PositionOnlyParent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionOnlyParent : MonoBehaviour {
+
+	[SerializeField]
+	private Transform positionParent;
+	private Vector3 relativePosition;
+
+	// Use this for initialization
+	void Start () {
+		relativePosition = transform.position - positionParent.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = positionParent.position + relativePosition;
+	}
+}
